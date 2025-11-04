a03d4b3796d981fe1e03b26989d32a140c35753b
Ulises <u.ivalero@hotmail.com>
2025-11-04 16:57:01 -0300

    v.54 Emailjs corrections

--- src/components/ui/Itineraries.jsx
+++ src/components/ui/Itineraries.jsx
@@ -5,28 +5,97 @@ const agencies = [
   { localidad: "Saavedra", agencia: "Puente Saavedra", direccion: "Zufriategui 1512 – Vte. Lopez", telefono: "4796-4241" },
   { localidad: "Morón", agencia: "N y C Travel", direccion: "Av. Rivadavia 17999 – Morón", telefono: "4627-7794" },
   { localidad: "San Justo", agencia: "Bru Bus", direccion: "Av. Arturo Illia 2582 – San Justo Centro", telefono: "4484-3610" },
-  { localidad: "San Justo", agencia: "Bru Bus Rotonda", direccion: "Monseñor Bufano 3457 – San Justo Centro", telefono: "1166543555" },
+  { localidad: "San Justo", agencia: "Bru Bus Rotonda", direccion: "Monseñor Bufano 3457 – San Justo Rotonda", telefono: "" },
   { localidad: "San Martín", agencia: "Reta Tur", direccion: "Avellaneda 3180 – Billinghust", telefono: "1166543555" }
 ]
 
-const stops = [
-  {hora: "21.30", parada:"Puente Saavedra", direccion:"Zufriategui 1512 – Vte. Lopez"},
-  {hora: "22.00", parada:"San Martin", direccion:"25 de Mayo esquina Gral. Paz (Carrefour)"},
-  {hora: "23.00", parada:"Morón", direccion:"Terminal de Omnibus – Av.Rivadavia 17417 – Morón"},
-  {hora: "23.50", parada:"San Justo Bru Bus", direccion:"Monseñor Bufano 3457 – San Justo"},
-  {hora: "", parada:"Ciudadela", direccion:"Av. Rivadavia 12300 "},
-  {hora: "", parada:"Ramos Mejía", direccion:"Av. Rivadavia 13764 "},
-  {hora: "", parada:"Tres Arroyos", direccion:"Av. San Martin entre Av. Almafuerte y La Rioja (Terminal de Tres Arroyos)"},
-  {hora: "", parada:"Oriente", direccion:"Av. St. María entre Alvear y Av. San Martin"},
-  {hora: "", parada:"", direccion:""},
+const stopsBsAsReta = [
+  { hora: "21.30", parada: "Puente Saavedra", direccion: "Zufriategui 1512 – Vte. Lopez" },
+  { hora: "22.00", parada: "San Martin", direccion: "25 de Mayo esquina Gral. Paz (Carrefour)" },
+  { hora: "22.15", parada: "Ciudadela", direccion: "Av. Rivadavia 12300" },
+  { hora: "22.25", parada: "Ramos Mejía", direccion: "Av. Rivadavia 13764" },
+  { hora: "23.00", parada: "Morón", direccion: "Terminal de Omnibus – Av.Rivadavia 17417 – Morón" },
+  { hora: "23.50", parada: "San Justo Bru Bus", direccion: "Monseñor Bufano 3457 – San Justo" },
+  { hora: "06.00", parada: "Tres Arroyos", direccion: "Av. San Martin entre Av. Almafuerte y La Rioja (Terminal de Tres Arroyos)" },
+  { hora: "07.00", parada: "Oriente", direccion: "Av. St. María entre Alvear y Av. San Martin" },
+]
+
+const stopsRetaBsAs = [
+  { hora: "21.45", parada: "Oriente", direccion: "Av. St. María entre Alvear y Av. San Martin" },
+  { hora: "22.30", parada: "Tres Arroyos", direccion: "Av. San Martin entre Av. Almafuerte y La Rioja (Terminal de Tres Arroyos)" },
+  { hora: "05.00", parada: "San Justo Bru Bus", direccion: "Monseñor Bufano 3457 – San Justo" },
+  { hora: "05.30", parada: "Morón", direccion: "Terminal de Omnibus – Av.Rivadavia 17417 – Morón" },
+  { hora: "06.00", parada: "San Martin", direccion: "25 de Mayo esquina Gral. Paz (Carrefour)" },
+  { hora: "06.00", parada: "Ciudadela", direccion: "Av. Rivadavia 12300" },
+  { hora: "06.00", parada: "Ramos Mejía", direccion: "Av. Rivadavia 13764" },
+  { hora: "06.30", parada: "Puente Saavedra", direccion: "Zufriategui 1512 – Vte. Lopez" },
 ]
 
 const Itineraries = () => {
   return (
     <Container>
       <div className="py-20 h-full">
-        <h2 className="w-full text-center font-h3 font-semibold text-5xl pb-5 text-dblue">¿Donde comprar mi pasaje?</h2>
-        <p className="w-full text-center font-p font-semibold text-lg pb-10 text-gray-700">Recorda que tambien podes adquirirlo mediante Plataforma 10</p>
+
+                <div className="mt-5">
+          <h2 className="w-full text-center font-h3 font-semibold text-5xl pb-10 text-dblue">
+            Paradas del recorrido
+          </h2>
+
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+            
+            <div>
+              <h3 className="text-center text-3xl font-semibold text-gold pb-5">Buenos Aires → Reta</h3>
+
+              <div className="hidden md:flex font-semibold text-gray-700 bg-gray-100 p-3 rounded-lg">
+                <div className="w-1/5"><h1 className="font-h1 font-medium text-dblue text-xl">Hora</h1></div>
+                <div className="w-1/3"><h1 className="font-h1 font-medium text-dblue text-xl">Parada</h1></div>
+                <div className="w-1/2"><h1 className="font-h1 font-medium text-dblue text-xl">Dirección</h1></div>
+              </div>
+
+              <div className="space-y-4 pt-3">
+                {stopsBsAsReta.map((item, idx) => (
+                  <div
+                    key={idx}
+                    className="flex flex-col space-y-3 md:space-y-0 md:flex-row md:items-center bg-white shadow-sm hover:shadow-md transition-shadow p-4 rounded-lg"
+                  >
+                    <div className="w-full md:w-1/5 font-semibold text-dblue">{item.hora}</div>
+                    <div className="w-full md:w-1/3 font-bold text-gold">{item.parada}</div>
+                    <div className="w-full md:w-1/2 text-gray-700">{item.direccion}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div>
+              <h3 className="text-center text-3xl font-semibold text-gold pb-5">Reta → Buenos Aires</h3>
+
+              <div className="hidden md:flex font-semibold text-gray-700 bg-gray-100 p-3 rounded-lg">
+                <div className="w-1/5"><h1 className="font-h1 font-medium text-dblue text-xl">Hora</h1></div>
+                <div className="w-1/3"><h1 className="font-h1 font-medium text-dblue text-xl">Parada</h1></div>
+                <div className="w-1/2"><h1 className="font-h1 font-medium text-dblue text-xl">Dirección</h1></div>
+              </div>
+
+              <div className="space-y-4 pt-3">
+                {stopsRetaBsAs.map((item, idx) => (
+                  <div
+                    key={idx}
+                    className="flex flex-col space-y-3 md:space-y-0 md:flex-row md:items-center bg-white shadow-sm hover:shadow-md transition-shadow p-4 rounded-lg"
+                  >
+                    <div className="w-full md:w-1/5 font-semibold text-dblue">{item.hora}</div>
+                    <div className="w-full md:w-1/3 font-bold text-gold">{item.parada}</div>
+                    <div className="w-full md:w-1/2 text-gray-700">{item.direccion}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* SECCIÓN DE AGENCIAS */}
+        <h2 className="w-full text-center font-h3 font-semibold text-5xl pb-5 text-dblue mt-20">¿Dónde comprar mi pasaje?</h2>
+        <p className="w-full text-center font-p font-semibold text-lg pb-10 text-gray-700">
+          Recordá que también podés adquirirlo mediante Plataforma 10
+        </p>
 
         <div className="hidden md:flex font-semibold text-gray-700 bg-gray-100 p-3 rounded-lg">
           <div className="w-1/5"><h1 className="font-h1 font-medium text-dblue text-2xl">Localidad</h1></div>
@@ -56,6 +125,8 @@ const Itineraries = () => {
             </div>
           ))}
         </div>
+
+
       </div>
     </Container>
   )
